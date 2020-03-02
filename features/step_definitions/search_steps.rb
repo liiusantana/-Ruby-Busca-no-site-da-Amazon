3fe7902a7b9a5c8aafcb4f62d977c3df08d22aab
liiusantana <liu_lsa@hotmail.com>
2020-03-02 00:49:27 -0300

    versão 0.1

---
+++ features/step_definitions/search_steps.rb
@@ -0,0 +1,21 @@
+
+
+Given("I navigate to www.amazon.com") do                                                                                             
+    visit 'https://www.amazon.com'                                                     
+  end                                                                                                                                  
+                                                                                                                                       
+  When("I select the option “Books” in the dropdown next to the search text input criteria.") do                                       
+    pending # Write code here that turns the phrase above into concrete actions                                                        
+  end                                                                                                                                  
+                                                                                                                                       
+  Then("I search for “Test automation”.") do                                                                                           
+    pending # Write code here that turns the phrase above into concrete actions                                                        
+  end                                                                                                                                  
+                                                                                                                                       
+  Then("I select the cheapest book of the page without using any sorting method available.") do                                        
+    pending # Write code here that turns the phrase above into concrete actions                                                        
+  end                                                                                                                                  
+                                                                                                                                       
+  When("I reach the detailed book page, I check if the name in the header is the same name of the book that I select previously.") do  
+    pending # Write code here that turns the phrase above into concrete actions                                                        
+  end                                                                                                                                  
---
+++ features/support/env.rb
@@ -0,0 +1,6 @@
+require 'capybara'
+require 'capybara/cucumber'
+
+Capybara.configure do |config|
+ config.default_driver = :selenium_chrome
+end
